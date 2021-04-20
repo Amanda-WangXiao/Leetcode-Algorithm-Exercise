4620036a236951ca4828b46eff64d92e32d21bc7
Amanda-WangXiao <amandawang0227@126.com>
2021-04-20 13:29:42 +0800

    update 28

--- Java/28_ImplementStrStr().java
+++ Java/28_ImplementStrStr().java
@@ -1,27 +1,54 @@
-class Solution {	//	0 ms	39.7 MB
+class Solution {	//	1 ms	38.5 MB
 	public int strStr(String haystack, String needle) {
-		int l1=haystack.length();
-		int l2=needle.length();
-		if(l2==0) return 0;
-		if(l1<l2) return -1;
-		for(int i=0;i<(l1-l2+1);i++){
-			if(haystack.substring(i,(i+l2)).equals(needle)) return i;
+		int haystack_len = haystack.length();
+		int needle_len = needle.length();
+
+		if(needle_len==0) return 0;
+		if(needle_len>haystack_len) return -1;
+
+		for(int i=0;i+needle_len<=haystack_len;i++){
+			if(haystack.substring(i,i+needle_len).equals(needle)) return i;
 		}
 		return -1;
 	}
 }
 
-class Solution {	//	0 ms	39.7 MB
+class Solution {	//	0 ms	38.3 MB
 	public int strStr(String haystack, String needle) {
-		int l1=haystack.length();
-		int l2=needle.length();
-		if(l2==0) return 0;
-		if(l1<l2) return -1;
-		for(int i=0;i<(l1-l2+1);i++){
+		int haystack_len = haystack.length();
+		int needle_len = needle.length();
+
+		if(needle_len==0) return 0;
+		if(needle_len>haystack_len) return -1;
+
+		for(int i=0;i+needle_len<=haystack_len;i++){
 			if(haystack.charAt(i)==needle.charAt(0)){
-				if(haystack.substring(i,(i+l2)).equals(needle)) return i;
+				if(haystack.substring(i,i+needle_len).equals(needle)) return i;
 			}
 		}
+		return -1;
+	}
+}
+
+class Solution {	//	2 ms	36.8 MB
+	public int strStr(String haystack, String needle) {
+		int haystack_len = haystack.length();
+		int needle_len = needle.length();
+
+		if(needle_len==0) return 0;
+		if(needle_len>haystack_len) return -1;
+
+		for(int i=0;i+needle_len<=haystack_len;i++){
+			int j=0;
+			if(haystack.charAt(i)==needle.charAt(0)){
+				while(j<needle_len){
+					if(haystack.charAt(i+j)!=needle.charAt(j)) break;
+					j++;
+				}
+				if(j>=needle_len) return i;
+			}
+		}
+
 		return -1;
 	}
 }
