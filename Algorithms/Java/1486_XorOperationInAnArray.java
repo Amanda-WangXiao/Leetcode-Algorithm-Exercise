ad742cbd19582b8dcc677a8b1c055391431a983e
Amanda-WangXiao <amandawang0227@126.com>
2021-05-07 10:08:54 +0800

    update 1486

--- Algorithms/Java/1486_XorOperationInAnArray.java
+++ Algorithms/Java/1486_XorOperationInAnArray.java
@@ -1,27 +1,10 @@
-class Solution {
-    public int[] twoSum_1(int[] nums, int target) {   //Brute Solution
-        int i,j;
-        for(i=0;i<nums.length;i++){
-            for(j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target) return new int[] {i,j};
-            }
+class Solution {    //  0 ms    35.4 MB
+    public int xorOperation(int n, int start) {
+        if(n==1) return start;
+        int xor = start;
+        for(int i=1;i<n;i++){
+            xor = xor^(start + 2*i);
         }
-        throw new IllegalArgumentException("Error");
+        return xor;
     }
-
-    public int[] twoSum_2(int[] nums, int target) {  //Hash Table
-        int i;
-        Map<Integer,Integer> m=new HashMap<>();
-        for(i=0;i<nums.length;i++) m.put(nums[i],i);
-
-        for(i=0;i<nums.length;i++){
-            int complement=target-nums[i];
-            if(m.containsKey(complement) && m.get(complement)!=i)
-                return new int[] {i,m.get(complement)};
-        }
-
-        throw new IllegalArgumentException("Error");
-    }
-
-    }
-
+}
